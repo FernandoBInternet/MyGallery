3896b40b7ee2d3cfe15be6cb80daddf76ed477d2
Fernando BInternet <fernandobinternetweb@gmail.com>
2020-06-26 17:09:12 -0400

    My gallery ready to deploy...

--- src/mongo/connection.js
+++ src/mongo/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const connection = mongoose.connect("mongodb://localhost:27017/mygallery", {
+const connection = mongoose.connect("mongodb://uwukoqedat5s9zghf8ag:1TAUENboQYuQI8RN8XWB@bvddcx2fmgx4cuo-mongodb.services.clever-cloud.com:27017/bvddcx2fmgx4cuo", {
 
 	useNewUrlParser: true,
 	useUnifiedTopology: true
